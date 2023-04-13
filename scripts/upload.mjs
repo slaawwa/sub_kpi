e3c47b80580cc39ab23ed5004b7ca4679f2239d7
Slaawwa <slaawwa@gmail.com>
2023-04-14 01:09:08 +0300

    Modify scripts js to mjs

--- scripts/upload.mjs
+++ scripts/upload.mjs
@@ -1,11 +1,22 @@
-const { post } = require('request-promise')
-const { exists, createReadStream } = require('fs')
-const { promisify } = require('util')
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+import RP from 'request-promise'
+import { stat, createReadStream } from 'node:fs'
+import { promisify } from 'util'
 
-const existsAsync = promisify(exists);
 
+const statAsync = promisify(stat)
 
-module.exports = async (filePath, {
+const existsAsync = fPath => statAsync(fPath)
+  .then(({ size = 0 } = {}) => size > 0)
+  .catch(err => err?.code !== 'ENOENT')
+
+const { post } = RP;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default async (filePath, {
   to,
   type = 'zip',
   project,
@@ -16,7 +27,6 @@ module.exports = async (filePath, {
   try {
     let has = await existsAsync(filePath)
     const file = has ? filePath : `${__dirname}/${filePath}`
-    console.log(' - file:14 >', file)
     has = await existsAsync(file)
     console.log(' - has:14 >', has) // eslint-disable-line no-console
     if (!has || !project) {
@@ -37,7 +47,7 @@ module.exports = async (filePath, {
     if (data !== 'true') {
       throw new Error(data);
     }
-    console.log(' - result:81 >', data, ) // eslint-disable-line no-console
+    console.log(' - result:81 >', data) // eslint-disable-line no-console
     return true
   } catch (err) {
     console.log('Error:::', `${err?.message || err}`) // eslint-disable-line no-console
